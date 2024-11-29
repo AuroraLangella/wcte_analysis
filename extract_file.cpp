48cb41aae9365409905da0527b89a35f527c80cc
alangella <alangella@na.infn.it>
2024-11-29 18:12:07 +0100

    Riusciamo a caricare le cariche di tutti gli eventi di una time slice (1s) in un vettore

--- extract_file.cpp
+++ extract_file.cpp
@@ -14,28 +14,24 @@ using namespace std::filesystem;
 
 int main(){
         
-    /*if (gSystem->Load("/opt/ToolFrameworkCore/lib/libStore.so") < 0) {
-        printf("Failed to load library\n");
-    } else {
-        printf("Library loaded successfully\n");
-    }*/      
+        
     
     std::string directoryPath = "/storage/wcte-data/run045/";
     
     
     int processedFiles = 0;
 
-    int maxFiles = 2;
+    int maxFiles = 1;
     
     
-    for (const auto& entry : std::filesystem::directory_iterator(directoryPath)) {
-        cout<<"loop iniziato"<<endl;
+    for (const auto& entry : directory_iterator(directoryPath)) {
+        
 
-        if (entry.is_regular_file()) { // Assicurati che sia un file
-            cout<<"file regolare"<<endl;
-            std::string filePath = entry.path().string();
+        if (entry.is_regular_file()) { // Assicurati che sia un file regolare
             
-            std::cout << "Processing file: " << filePath << std::endl;
+            string filePath = entry.path().string();
+            
+            cout << "Processing file: " << filePath << std::endl;
             
         
             WCTERawData p;
@@ -53,13 +49,38 @@ int main(){
             //p.readout_windows[16].Print()
 
             //Apri un singolo time slice (cioè la 16) e da questo prendi l'evento hkmpmt_hits[0]
-            p.readout_windows[16].hkmpmt_hits[0].Print();
+            //p.readout_windows[16].hkmpmt_hits[0].Print();
             //Leggi i dati raw di questo evento 
             //p.readout_windows[16].hkmpmt_hits[0].Dump()
-            //Leggi qual
-            //p.readout_windows[16].hkmpmt_hits[4].header.GetCardID()
+            //int charge[sizeof(p.readout_windows[16])];
+            
 
-            //p.readout_windows[16].hkmpmt_hits[0].footer.GetCharge()
+            //int n = sizeof(p.readout_windows[16]) / sizeof(p.readout_windows[16].hkmpmt_hits[0]);   
+            //cout << n << endl;              
+            //int* charge = new int[n];
+            std::vector<int> charge;
+            //cout<<"Size of charge:"<< sizeof(charge)<<endl<<endl;
+            for (int i=0; i<sizeof(p.readout_windows[16]); i++){
+                cout<<"Valore carica dell'elemento"<<i<<" : "<<p.readout_windows[16].hkmpmt_hits[i].footer.GetCharge()<<endl;
+                //charge[i] = p.readout_windows[16].hkmpmt_hits[i].footer.GetCharge();
+                //cout<< p.readout_windows[16].hkmpmt_hits[i].footer.GetCharge()<<endl;
+                
+                /*if (p.readout_windows[16].hkmpmt_hits[i].footer.GetCharge() == 4095){
+
+                    cout<< p.readout_windows[16].hkmpmt_hits[i].subhits<<endl;
+                }*/
+               charge.push_back(p.readout_windows[16].hkmpmt_hits[i].footer.GetCharge());
+            }
+
+            cout << "Charge vector filled"<<endl;
+            for (int val : charge) {
+                std::cout << val << endl;
+            }
+
+            //Leggi quale pmt ha visto l'evento
+            p.readout_windows[16].hkmpmt_hits[4].header.GetCardID();
+            // Leggi la carica dell'evento
+            p.readout_windows[16].hkmpmt_hits[0].footer.GetCharge();
                       
             processedFiles++;
             
