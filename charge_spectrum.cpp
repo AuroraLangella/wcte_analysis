c6e0c4f32c39710a11e717c7675146d797aba1d7
alangella <alangella@na.infn.it>
2024-12-01 23:25:59 +0100

    Modifica al range e binnaggio istogrammi

--- charge_spectrum.cpp
+++ charge_spectrum.cpp
@@ -19,6 +19,9 @@ int main() {
     TTree *tree = (TTree*)inputFile->Get("data");
 
     int charge, PMT_ID;
+    int ped[] = {260,0,257,259,257,260,256,260,258,260,259,260,260,257,259,259,261,258,258};
+    int cont = 0;
+    
     tree->SetBranchAddress("charge", &charge);
     tree->SetBranchAddress("PMT_ID", &PMT_ID);
 
@@ -33,21 +36,23 @@ int main() {
     }
 
     
-    TFile *outputFile = new TFile("histograms_run045.root", "RECREATE");
-
+    TFile *outputFile = new TFile("histograms_run045.root", "RECREATE");    
     
     TCanvas *canvas = new TCanvas("canvas", "Charge Spectrum (logscale)", 800, 600);
     canvas->SetLogy();
-
     // const auto prende direttamente la dimensione della mappa e permette di ciclare sopra sia 
     //la chiave (pmtID che è un int) che la carica (che è un vector<int>)
     for (const auto& entry : chargeByPMT) {
+
         int pmt = entry.first;
         const vector<int>& charges = entry.second;
 
         
         int maxCharge = *max_element(charges.begin(), charges.end());
-        int nBins = TMath::Min(100, maxCharge / 10 + 1); 
+        //int nBins = TMath::Min(100, maxCharge / 10 + 1); 
+        int nBins = maxCharge-ped[cont];
+
+        
 
 
         string histName = "hist_PMT_" + to_string(pmt);
@@ -66,11 +71,11 @@ int main() {
             hist_zoom->GetXaxis()->CenterTitle();
             hist_zoom->GetYaxis()->CenterTitle();        
             hist_zoom->Draw();
-            canvas->SaveAs((histName_zoom + "_log.png").c_str());
+            canvas->SaveAs(("./plots/"+histName_zoom + "_log.png").c_str());
             canvas->Clear();
         }
 
-        TH1F *hist = new TH1F(histName.c_str(), histTitle.c_str(), nBins, -1000, maxCharge + 1000);
+        TH1F *hist = new TH1F(histName.c_str(), histTitle.c_str(), nBins, ped[cont], maxCharge);
         
         
         for (int c : charges) {
@@ -85,8 +90,10 @@ int main() {
 
         
         hist->Draw();
-        canvas->SaveAs((histName + "_log.png").c_str());
+        canvas->SaveAs(("./plots/"+histName + "_log.png").c_str());
         hist->Write(); 
+        canvas->Clear();
+        cont = cont+1;
     }
 
     
